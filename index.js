78cdbfd3e117ca92ca84bbf80fbbe7f913686cc7
Alex Hamilton <alex@bflatstudios.com>
2015-12-24 15:33:24 -0600

    Add body parsing and recipe creation handling.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
 var Recipe = require("./models/recipe");
 var app = express();
 
+app.use(bodyParser.json());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
@@ -27,6 +29,43 @@ app.get("/recipe/:slug", function(req, res, next) {
 
 app.get("/add-recipe", function(req, res, next) {
 	res.render("add-recipe");
+});
+
+app.post("/add-recipe", function(req, res, next) {
+	try {
+		var name = req.body.name;
+		var source = req.body.source || undefined;
+		var url = req.body.url || undefined;
+		var categories = (req.body.categories || "").split(",");
+		var tags = (req.body.tags || "").split(",");
+		var ingredients = (req.body.ingredients || "").replace("\r", "").split("\n");
+		var directions = req.body.directions.replace("\r", "").split("\n");
+		var prepTime = req.body.prep || undefined;
+		var cookTime = req.body.cook || undefined;
+		var serves = req.body.serves || undefined;
+
+		var recipe = new Recipe({name: name});
+		if(source) recipe.source = source;
+		if(url) recipe.url = url;
+		recipe.categories = categories;
+		recipe.tags = tags;
+		recipe.ingredients = ingredients;
+		recipe.directions = directions;
+		if(prepTime) recipe.time = {prep: prepTime};
+		if(cookTime) {
+			if(recipe.time) {
+				recipe.time.cook = cookTime;
+			} else {
+				recipe.time = {cook: cookTime};
+			}
+		}
+		recipe.save(function(err) {
+			if(err) return next(err);
+			res.redirect("/recipe/" + recipe.slug);
+		});
+	} catch(e) {
+		return next(err);
+	}
 });
 
 module.exports = app;
