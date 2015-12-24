334cbdee218796669ad99539edbe4777023ddc82
Alex Hamilton <alex@bflatstudios.com>
2015-12-24 14:22:08 -0600

    Add /recipes route.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require("express");
 var path = require("path");
+var Recipe = require("./models/recipe");
 var app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
+
+app.get("/recipes", function(req, res, next) {
+	return Recipe.find({}, function(err, recipes) {
+		if(err) return next(err);
+		return res.render("recipe-list", {
+			recipes: recipes
+		});
+	});
+});
 
 module.exports = app;
