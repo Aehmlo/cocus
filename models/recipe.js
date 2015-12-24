5428091f69a04dfdea75fbee3c2e8b1345c7f3a2
Alex Hamilton <alex@bflatstudios.com>
2015-12-24 14:02:38 -0600

    Add recipe model.

---
+++ models/recipe.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+
+var recipeSchema = mongoose.Schema({
+	name: { type: String, required: true, unique: true},
+	source: String,
+	url: String,
+	categories: [String],
+	tags: [String],
+	ingredients: [String],
+	directions: [String],
+	time: {
+		prep: Number,
+		cook: Number
+	},
+	serves: Number,
+	slug: {type: String, required: true, unique: true}
+});
+
+recipeSchema.virtual("time.total").get(function() {
+	return this.time.prep + this.time.cook || undefined;
+});
+
+recipeSchema.pre("validate", function(next) {
+	try {
+		var parts = this.name.replace(/[^A-Za-z0-9 ]/g, "").split(" "); // ASCII characters only
+		this.slug = parts.join("-").toLowerCase();
+		console.log("Created slug", this.slug, "for recipe name", this.name);
+		next();
+	} catch(err) {
+		return next(err);
+	}
+});
+
+module.exports = mongoose.model("Recipe", recipeSchema);
