24dfc9385b58dacfe6c2dd0d519f341679f7368a
Alex Hamilton <alex@bflatstudios.com>
2015-12-25 12:56:53 -0600

    Add search capabilities.

--- models/recipe.js
+++ models/recipe.js
@@ -23,6 +23,21 @@ recipeSchema.virtual("time.total").get(function() {
 	return this.time.prep + this.time.cook || undefined;
 });
 
+recipeSchema.index({
+	name: "text",
+	source: "text",
+	tags: "text"
+});
+
+recipeSchema.statics.search = function(query, callback) {
+	return this.model("Recipe").find({
+		{$text: { $search: query } },
+		{score: { $meta: "textScore" }}
+	}).sort({
+		score: { $meta: "textScore" }
+	}).exec(callback);
+};
+
 recipeSchema.pre("validate", function(next) {
 	try {
 		var parts = this.name.replace(/[^A-Za-z0-9 ]/g, "").split(" "); // Alphanumeric characters (and space) only
